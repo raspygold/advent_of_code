5dbcb0db0d5baa40f07fbe0587b0b8869cea02c4
Ross Baird <raspygold@gmail.com>
2015-12-18 23:03:55 +1300

    Day 10 solutions

---
+++ 10/solution-1.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+input = "1321131112"
+
+40.times do
+  result = ""
+  last_chars = []
+  input.chars.each.with_index do |char, i|
+    last_chars << char and next if (last_chars.include?(char) || last_chars.empty?)
+
+    result += "#{last_chars.size}#{last_chars.first}"
+    last_chars.clear << char
+  end
+
+  result += "#{last_chars.size}#{last_chars.first}"
+
+  input = result
+end
+
+puts input.length
+# => 492982
