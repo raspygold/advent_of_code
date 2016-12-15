6fe02b950569022a4cf91e6e832b1209f028c191
Ross Baird <raspygold@gmail.com>
2016-12-15 19:47:04 +1300

    Day 15 part 1 solution

---
+++ 2016/15/part1.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+file_path = File.expand_path("../input", __FILE__)
+input     = File.readlines(file_path)
+
+class Sculpture
+  class Disc < Struct.new(:positions, :starts_at); end
+
+  def initialize(input)
+    @discs = input.map do |line|
+      positions, starts_at = /(\d+) positions; at time=\d+, it is at position (\d+)/.match(line).captures.map(&:to_i)
+
+      Disc.new(positions, starts_at)
+    end
+  end
+
+  def open_after?(seconds)
+    @discs.each.with_index do |disc, i|
+      current_position = (disc.starts_at + seconds + i + 1) % disc.positions
+      return false unless current_position == 0
+    end
+    return true
+  end
+
+  def simulate
+    i = 1
+    loop do
+      return i if open_after?(i)
+      i += 1
+    end
+  end
+end
+
+sculpture = Sculpture.new(input)
+
+puts "", "The first time you can press the button to get a capsule is: #{sculpture.simulate}"
