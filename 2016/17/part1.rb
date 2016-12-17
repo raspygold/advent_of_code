573bf9cd3a58bda75b2ea87a3b3a38aa2e981113
Ross Baird <raspygold@gmail.com>
2016-12-17 19:41:45 +1300

    Day 17 part 1 solution

---
+++ 2016/17/part1.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+file_path = File.expand_path("../input", __FILE__)
+$input     = File.read(file_path).strip
+
+require 'digest'
+
+def determine_next_moves(path, position)
+  print "."
+
+  if position == [3,3]
+    return path
+  end
+
+  digest = Digest::MD5.hexdigest("#{$input}#{path}")
+  up, down, left, right = digest.split("")[0..3]
+  [up, down, left, right].map.with_index do |value, i|
+    if value =~ /[b-z]/
+      position_modifier = [[0,-1], [0,1], [-1,0], [1,0]][i]
+      new_position = [position[0] + position_modifier[0], position[1] + position_modifier[1]]
+      new_path = path + ["U", "D", "L", "R"][i]
+
+      if new_position.all? { |c| c.between?(0, 3) } # Inside the grid
+        determine_next_moves(new_path, new_position)
+      end
+    end
+  end.compact
+end
+
+shortest_path = determine_next_moves("", [0,0]).flatten.compact.min_by(&:length)
+
+puts "", "The shortest path to the vaults is: #{shortest_path}"
