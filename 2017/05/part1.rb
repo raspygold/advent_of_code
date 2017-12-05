c404f6ac5097044702601302609032aaf43e8a8b
Ross Baird <raspygold@gmail.com>
2017-12-05 21:36:24 +1300

    Day 05 challenge part 1 & 2

---
+++ 2017/05/part1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+file_path = File.expand_path("../input", __FILE__)
+input     = File.readlines(file_path)
+
+instructions = input.map { |line| line.strip.to_i }
+
+next_step = steps_taken = 0
+
+loop do
+  next_instruction = instructions[next_step]
+  instructions[next_step] += 1
+
+  next_step += next_instruction
+  steps_taken += 1
+
+  break if next_step < 0 || next_step >= instructions.length
+end
+
+puts "Steps to exit: #{steps_taken}"
