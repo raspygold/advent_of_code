601431c9c6f8c7f1a1ce42236539a7e58e5010c5
Ross Baird <483090+raspygold@users.noreply.github.com>
2019-12-12 07:27:05 +1300

    WIP intcode refactor

--- 2019/09/part1.rb
+++ 2019/09/part1.rb
@@ -8,78 +8,129 @@ class Day9P1 < Puzzle
     }
   end
 
-  def resolve(m, a, prog, r_idx)
-    {
-      0 => prog[a],
-      1 => a,
-      2 => prog[r_idx + a],
-    }[m]
+  def solve(input, testing: false)
+    instructions = input.split(',').map(&:to_i)
+    instructions += ([0] * 1_000_000) # pad program memory heap
+    instructions.freeze
+    intcode_program = IntcodeProgram.new(instructions.dup, [1])
+    intcode_program.run
+
+    puts intcode_program.outputs.inspect
+    intcode_program.outputs.last
   end
 
-  def solve(input, testing: false)
-    program = input.split(',').map(&:to_i)
-    program += ([0] * 1_000_000) # pad program memory heap
-    prog_input  = [1]
-    prog_output = []
+  class IntcodeProgram
+    attr_reader   :outputs
 
-    idx = r_idx = 0
-    loop do
-      opcode = program[idx]
-      m = Array(opcode.digits[2..-1])
-      m << 0 while m.size < 3 # pad it
-      opcode = opcode % 100
+    OPS = {
+      1  => :add,
+      2  => :mutliply,
+      3  => :ingest,
+      4  => :output,
+      5  => :jump_if_true,
+      6  => :jump_if_false,
+      7  => :less_than,
+      8  => :equals,
+      9  => :adjust_relative_idx
+    }
 
-      case opcode
-      when 1 # Add
-        a, b, c = program.slice(idx+1, 3)
-        program[c] = resolve(m[0], a, program, r_idx) + resolve(m[1], b, program, r_idx)
-        idx += 4
-      when 2 # Multiply
-        a, b, c = program.slice(idx+1, 3)
-        program[c] = resolve(m[0], a, program, r_idx) * resolve(m[1], b, program, r_idx)
-        idx += 4
-      when 3 # Ingest input
-        a = program.slice(idx+1)
-        program[a] = prog_input.shift
-        idx += 2
-      when 4 # Output
-        a = program.slice(idx+1)
-        # puts "Outputing:"
-        # puts [m[0], a, r_idx, resolve(m[0], a, program, r_idx)].inspect
-        prog_output << resolve(m[0], a, program, r_idx)
-        idx += 2
-      when 5 # Jump-if-true
-        a, b = program.slice(idx+1, 2)
-        if resolve(m[0], a, program, r_idx) != 0
-          idx = resolve(m[1], b, program, r_idx)
-        else
-          idx += 3
-        end
-      when 6 # Jump-if-false
-        a, b = program.slice(idx+1, 2)
-        if resolve(m[0], a, program, r_idx) == 0
-          idx = resolve(m[1], b, program, r_idx)
-        else
-          idx += 3
-        end
-      when 7 # Less than
-        a, b, c = program.slice(idx+1, 3)
-        program[c] = resolve(m[0], a, program, r_idx) < resolve(m[1], b, program, r_idx) ? 1 : 0
-        idx += 4
-      when 8 # Equals
-        a, b, c = program.slice(idx+1, 3)
-        program[c] = resolve(m[0], a, program, r_idx) == resolve(m[1], b, program, r_idx) ? 1 : 0
-        idx += 4
-      when 9 # Adjust relative base
-        a = program.slice(idx+1)
-        r_idx += resolve(m[0], a, program, r_idx)
-        idx += 2
-      when 99
-        break
+    def add(a,b,c); @program[c] = a + b; end
+    def mulitply(a,b,c); @program[c] = a * b; end
+    def ingest(a); @program[a] = @input.shift; end
+    def output(a); @outputs << a; end
+    def jump_if_true(a,b); @idx = b if a != 0; end
+    def jump_if_false(a,b); @idx = b if a == 0; end
+    def less_than(a,b,c); @program[c] = a < b ? 1 : 0; end
+    def equals(a,b,c); @program[c] = a == b ? 1 : 0; end
+    def adjust_relative_idx(a); @r_idx += a; end
+
+    def initialize(program, input)
+      @program = program
+      @input = Array(input)
+      @outputs = []
+    end
+
+    def run
+      @idx = 0
+      @r_idx = 0
+      loop do
+        instruction = @program[@idx]
+        puts "INSTR: #{instruction}"
+        m = Array(instruction.digits[2..-1])
+        m << 0 while m.size < 3 # pad it
+        opcode = instruction % 100
+
+        break if opcode == 99
+        # case opcode
+        # when 1,2,3,4,5,6 # Add
+          modes = Array(instruction.digits[2..-1])
+          modes << 0 while modes.size < 3 # pad it
+          fun = method(OPS[opcode])
+          args = @program.slice(@idx+1, fun.arity).map.with_index do |x, i|
+            {
+              0 => @program[x],
+              1 => x,
+              2 => @program[@r_idx + x],
+            }[modes[i]]
+          end
+          fun.call(*args)
+          @idx += fun.arity + 1
+          # exit
+        # when 2 # Multiply
+        #   a, b, c = @program.slice(@idx+1, 3)
+        #   @program[c] = resolve(m[0], a) * resolve(m[1], b)
+        #   @idx += 4
+        # when 3 # Ingest input
+        #   a = @program.slice(@idx+1)
+        #   @program[a] = @input.shift
+        #   @idx += 2
+        # when 4 # Output
+        #   a = @program.slice(@idx+1)
+        #   @outputs << resolve(m[0], a)
+        #   @idx += 2
+        # when 5 # Jump-if-true
+        #   a, b = @program.slice(@idx+1, 2)
+        #   if resolve(m[0], a) != 0
+        #     @idx = resolve(m[1], b)
+        #   else
+        #     @idx += 3
+        #   end
+        # when 6 # Jump-if-false
+        #   a, b = @program.slice(@idx+1, 2)
+        #   if resolve(m[0], a) == 0
+        #     @idx = resolve(m[1], b)
+        #   else
+        #     @idx += 3
+        #   end
+        # when 7 # Less than
+        #   a, b, c = @program.slice(@idx+1, 3)
+        #   @program[c] = resolve(m[0], a) < resolve(m[1], b) ? 1 : 0
+        #   @idx += 4
+        # when 8 # Equals
+        #   a, b, c = @program.slice(@idx+1, 3)
+        #   @program[c] = resolve(m[0], a) == resolve(m[1], b) ? 1 : 0
+        #   @idx += 4
+        # when 9 # Adjust relative base
+        #   a = @program.slice(@idx+1)
+        #   @r_idx += resolve(m[0], a)
+        #   @idx += 2
+        # when 99
+        #   break
+        # end
       end
     end
 
-    puts prog_output.inspect
-    prog_output.last
+    # private
+    # def resolve(m, a)
+    #   {
+    #     0 => @program[a],
+    #     1 => a,
+    #     2 => @program[@r_idx + a],
+    #   }[m]
+    # end
+
+    # def handle_output(output)
+    #   @outputs << output
+    # end
   end
 end
