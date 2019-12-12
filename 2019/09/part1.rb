d2933729b4e4b4211678a9fe55e1896c04936e56
Ross Baird <483090+raspygold@users.noreply.github.com>
2019-12-12 22:17:57 +1300

    Refactor done, same issue :(

--- 2019/09/part1.rb
+++ 2019/09/part1.rb
@@ -10,7 +10,7 @@ class Day9P1 < Puzzle
 
   def solve(input, testing: false)
     instructions = input.split(',').map(&:to_i)
-    instructions += ([0] * 1_000_000) # pad program memory heap
+    instructions += ([0] * 100_000) # pad program memory heap
     instructions.freeze
     intcode_program = IntcodeProgram.new(instructions.dup, [1])
     intcode_program.run
@@ -20,29 +20,29 @@ class Day9P1 < Puzzle
   end
 
   class IntcodeProgram
-    attr_reader   :outputs
+    attr_reader :outputs
 
     OPS = {
       1  => :add,
-      2  => :mutliply,
+      2  => :multiply,
       3  => :ingest,
       4  => :output,
       5  => :jump_if_true,
       6  => :jump_if_false,
       7  => :less_than,
       8  => :equals,
-      9  => :adjust_relative_idx
+      9  => :adjust_relative_base
     }
 
     def add(a,b,c); @program[c] = a + b; end
-    def mulitply(a,b,c); @program[c] = a * b; end
+    def multiply(a,b,c); @program[c] = a * b; end
     def ingest(a); @program[a] = @input.shift; end
     def output(a); @outputs << a; end
     def jump_if_true(a,b); @idx = b if a != 0; end
     def jump_if_false(a,b); @idx = b if a == 0; end
     def less_than(a,b,c); @program[c] = a < b ? 1 : 0; end
     def equals(a,b,c); @program[c] = a == b ? 1 : 0; end
-    def adjust_relative_idx(a); @r_idx += a; end
+    def adjust_relative_base(a); @r_base += a; end
 
     def initialize(program, input)
       @program = program
@@ -52,85 +52,32 @@ class Day9P1 < Puzzle
 
     def run
       @idx = 0
-      @r_idx = 0
+      @r_base = 0
       loop do
         instruction = @program[@idx]
-        puts "INSTR: #{instruction}"
-        m = Array(instruction.digits[2..-1])
-        m << 0 while m.size < 3 # pad it
         opcode = instruction % 100
-
+        pre_instr_idx = @idx
         break if opcode == 99
-        # case opcode
-        # when 1,2,3,4,5,6 # Add
-          modes = Array(instruction.digits[2..-1])
-          modes << 0 while modes.size < 3 # pad it
-          fun = method(OPS[opcode])
-          args = @program.slice(@idx+1, fun.arity).map.with_index do |x, i|
-            {
-              0 => @program[x],
-              1 => x,
-              2 => @program[@r_idx + x],
+        fun = method(OPS[opcode])
+        modes = Array(instruction.digits[2..-1])
+        modes << 0 while modes.size < 3 # pad it
+        modes[-1] = 1 unless [3,5,6,9].include?(opcode)
+        # puts "INSTR: #{[instruction, @program.slice(@idx+1, fun.arity)].inspect}"
+        # puts "MODES: #{modes.inspect}"
+        args = @program[@idx+1, fun.arity].map.with_index do |x, i|
+          {
+            0 => @program[x],
+            1 => x,
+            2 => @program[@r_base + x],
             }[modes[i]]
           end
-          fun.call(*args)
-          @idx += fun.arity + 1
-          # exit
-        # when 2 # Multiply
-        #   a, b, c = @program.slice(@idx+1, 3)
-        #   @program[c] = resolve(m[0], a) * resolve(m[1], b)
-        #   @idx += 4
-        # when 3 # Ingest input
-        #   a = @program.slice(@idx+1)
-        #   @program[a] = @input.shift
-        #   @idx += 2
-        # when 4 # Output
-        #   a = @program.slice(@idx+1)
-        #   @outputs << resolve(m[0], a)
-        #   @idx += 2
-        # when 5 # Jump-if-true
-        #   a, b = @program.slice(@idx+1, 2)
-        #   if resolve(m[0], a) != 0
-        #     @idx = resolve(m[1], b)
-        #   else
-        #     @idx += 3
-        #   end
-        # when 6 # Jump-if-false
-        #   a, b = @program.slice(@idx+1, 2)
-        #   if resolve(m[0], a) == 0
-        #     @idx = resolve(m[1], b)
-        #   else
-        #     @idx += 3
-        #   end
-        # when 7 # Less than
-        #   a, b, c = @program.slice(@idx+1, 3)
-        #   @program[c] = resolve(m[0], a) < resolve(m[1], b) ? 1 : 0
-        #   @idx += 4
-        # when 8 # Equals
-        #   a, b, c = @program.slice(@idx+1, 3)
-        #   @program[c] = resolve(m[0], a) == resolve(m[1], b) ? 1 : 0
-        #   @idx += 4
-        # when 9 # Adjust relative base
-        #   a = @program.slice(@idx+1)
-        #   @r_idx += resolve(m[0], a)
-        #   @idx += 2
-        # when 99
-        #   break
-        # end
+        # puts [opcode, fun, args].inspect
+        fun.call(*args)
+        @idx += fun.arity + 1 unless @idx != pre_instr_idx # index was updated so leave it be
+        # puts "OUTPUTS: #{outputs.inspect}"
+        # puts ({ idx: @idx, r_base: @r_base }).inspect
+        # puts
       end
     end
-
-    # private
-    # def resolve(m, a)
-    #   {
-    #     0 => @program[a],
-    #     1 => a,
-    #     2 => @program[@r_idx + a],
-    #   }[m]
-    # end
-
-    # def handle_output(output)
-    #   @outputs << output
-    # end
   end
 end
