cc3d6493df39597613a1da8d2bdd110274e08620
Ross Baird <483090+raspygold@users.noreply.github.com>
2020-12-14 13:54:33 +1300

    Day 13

---
+++ 2020/13/part1.rb
@@ -0,0 +1,33 @@
+require_relative "../puzzle"
+
+class Day12P1 < Puzzle
+
+  class Bus
+    attr_accessor :id
+
+    def initialize(id)
+      @id = id
+    end
+
+    def next_time_after(t)
+      ((t / id) + 1) * id
+    end
+  end
+
+  def test_cases
+    { # {input => expected}
+      ['939',
+       '7,13,x,x,59,x,31,19'] => 295
+    }
+  end
+
+  def solve(input, testing: false)
+    earliest_time = input[0].to_i
+    bus_ids = input[1].split(",").map(&:to_i).select { |b| b > 0}
+    buses = bus_ids.map { |b| Bus.new(b) }
+
+    next_bus = buses.min_by { |b| b.next_time_after(earliest_time) }
+
+    next_bus.id * (next_bus.next_time_after(earliest_time) - earliest_time)
+  end
+end
---
+++ 2020/13/part2.rb
@@ -0,0 +1,46 @@
+require_relative "../puzzle"
+
+class Day12P1 < Puzzle
+
+  class Bus
+    attr_accessor :id, :seq
+
+    def initialize(id, seq)
+      @id = id
+      @seq = seq
+    end
+
+    def in_seq_after?(t)
+      (t + seq) % id == 0
+    end
+  end
+
+  def test_cases
+    { # {input => expected}
+      ['_', '17,x,13,19'] => 3417,
+      ['_', '67,7,59,61'] => 754018,
+      ['_', '67,x,7,59,61'] => 779210,
+      ['_', '67,7,x,59,61'] => 1261476,
+      ['_', '1789,37,47,1889'] => 1202161486,
+      ['939', '7,13,x,x,59,x,31,19'] => 1068781
+    }
+  end
+
+  def solve(input, testing: false)
+    buses = input[1].split(",").map(&:to_i).map.with_index do |b, i|
+      Bus.new(b, i) if b > 0
+    end.compact
+
+    largest_bus = buses.max_by { |b| b.id }
+    start_at = largest_bus.id
+
+    t = 0
+    interval = 1 # time between sequencial buses
+    buses.each do |bus|
+      t += interval until bus.in_seq_after?(t)
+      interval *= bus.id
+    end
+
+    t
+  end
+end
